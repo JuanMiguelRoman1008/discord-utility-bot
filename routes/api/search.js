5238d241e2666457b3b54a83bbdb27b00cf3dca5
Juan Miguel <nehstroman@gmail.com>
2022-05-17 22:46:36 +0800

    Replanned routes for Search

--- routes/api/search.js
+++ routes/api/search.js
@@ -9,27 +9,27 @@ router.post("/", (req, res) => {
     res.send("Add an LF request");
 });
 
-// @route  DELETE api/search/join/:id
+// @route  DELETE api/search/:id
 // @desc   Delete your LF Request
 // @access Private
 
-router.delete("/join/:id", (req, res) => {
+router.delete("/:id", (req, res) => {
     res.send("Delete your own LF request");
 });
 
-// @route  PUT api/search/join/:id
+// @route  POST api/search/:id/join/:playerID
 // @desc   Manually add someone to your request
 // @access Private
 
-router.put("/join/:id", (req, res) => {
+router.post(":id/join/:playerID", (req, res) => {
     res.send("Manually add someone to your LF Request");
 });
 
-// @route  POST api/search/join/:id
+// @route  DELETE api/search/:id/join/::playerID
 // @desc   Join/Unjoin an LF request
 // @access Private
 
-router.post("/join/:id", (req, res) => {
+router.delete(":id/join/:playerID", (req, res) => {
     res.send("Join/Unjoin an LF Request");
 });
 
