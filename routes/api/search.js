7ea5a8fd6625b1174870e954caccb887a51d3ec6
Juan Miguel <nehstroman@gmail.com>
2022-05-22 16:47:13 +0800

    Update leave request route to use database controllers

--- routes/api/search.js
+++ routes/api/search.js
@@ -68,36 +68,27 @@ router.post("/:searchID/player/:playerID", async (req, res) => {
 });
 
 // @route  DELETE api/search/:id/join/::playerID
-// @desc   Join/Unjoin an LF request
+// @desc   Unjoin search request
 // @access Private
 
-router.delete("/:id/player/:playerID", async (req, res) => {
+router.delete("/:searchID/player/:playerID", async (req, res) => {
     try {
-        const id = req.params.id;
-        const playerID = req.params.playerID;
-
-        const search = await Search.findById(id);
-        if (!search) {
-            return res.status(404).send(`Search ID: ${id} does not exist`);
-        }
-
-        if (!search.party.some((player) => player.user === playerID)) {
-            return res
-                .status(404)
-                .send(`Player: ${playerID} is not in Search: ${id}`);
-        }
-
-        let updatedParty = search.party.filter((player) => {
-            return player.user !== playerID;
-        });
-
-        search.party = updatedParty;
-        await search.save();
-        return res.send(search);
+        const search = await databaseControllers.leaveSearch(req.params);
+        res.send(search);
+        return;
     } catch (error) {
-        res.status(500).send(
-            `An error occured while trying to leave the search, ${error}`
-        );
+        if (error.message === "NOT_FOUND") {
+            res.status(404).send(`Search ID does not exist`);
+            return;
+        } else if (error.message === "PLAYER_NOT_FOUND") {
+            res.status(404).send(`Player ID is not in Search`);
+            return;
+        } else {
+            res.status(500).send(
+                `An error occured while trying to join the search`
+            );
+            return;
+        }
     }
 });
 
